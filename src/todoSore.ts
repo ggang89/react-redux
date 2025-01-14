25f820c6b8fe7568f12d077226fbe5e17160eaf2
ggang89 <ggang89@gmail.com>
2025-01-14 22:38:31 +0900

    createAction 사용 => type과 payload로 활용

--- src/todoSore.ts
+++ src/todoSore.ts
@@ -1,33 +1,42 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
 type ToDo = { id: string; todoTitle: string };
 type ToDos = ToDo[];
-type Action = { type: string; text: string; id: string  };
+type Action = { type: string; payload:string  };
 
- const addTodo = (text: string) => {
-  return {
-    type: "ADD_TODO",
-    text,
-  };
-};
- const deleteTodo = (id: string) => {
-  return {
-    type: "DELETE_TODO",
-    id,
-  };
-};
+//  const addTodo = (text: string) => {
+//   return {
+//     type: "ADD_TODO",
+//     text,
+//   };
+// };
+//  const deleteTodo = (id: string) => {
+//   return {
+//     type: "DELETE_TODO",
+//     id,
+//   };
+// };
+
+// 자동으로 type과 payload를 반환하는 함수를 만들어줌
+const addTodo = createAction("ADD"); 
+const deleteTodo = createAction("DELETE");
+
+//console.log( addTodo());  //  {type:'ADD',payload:'undefined'}
+
 
 const reducer = (state: ToDos = [], action: Action) => {
   switch (action.type) {
-    case "ADD_TODO":
+    case addTodo.type:
+      console.log(action)
       {
-        const newTodo: ToDo = { id: uuidv4(), todoTitle: action.text };
+        const newTodo: ToDo = { id: uuidv4(), todoTitle: action.payload };
         return [newTodo, ...state];
       }
       break;
-    case "DELETE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
+    case deleteTodo.type:
+      return state.filter((todo) => todo.id !== action.payload);
       break;
     default:
       return state;
