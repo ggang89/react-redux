42be07fecf59eb719a3374b7adfa09a5d60f164b
ggang89 <ggang89@gmail.com>
2025-01-14 23:53:10 +0900

    crateReducer 사용 => builder 콜백사용 // error

--- src/todoSore.ts
+++ src/todoSore.ts
@@ -1,10 +1,11 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
+import ToDo from "./ToDo";
 
 type ToDo = { id: string; todoTitle: string };
 type ToDos = ToDo[];
-type Action = { type: string; payload:string  };
+type Action = { type: string; payload: string };
 
 //  const addTodo = (text: string) => {
 //   return {
@@ -20,28 +21,39 @@ type Action = { type: string; payload:string  };
 // };
 
 // 자동으로 type과 payload를 반환하는 함수를 만들어줌
-const addTodo = createAction("ADD"); 
+const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
 //console.log( addTodo());  //  {type:'ADD',payload:'undefined'}
 
+// const reducer = (state: ToDos = [], action: Action) => {
+//   switch (action.type) {
+//     case addTodo.type:
+//       console.log(action)
+//       {
+//         const newTodo: ToDo = { id: uuidv4(), todoTitle: action.payload };
+//         return [newTodo, ...state];
+//       }
+//       break;
+//     case deleteTodo.type:
+//       return state.filter((todo) => todo.id !== action.payload);
+//       break;
+//     default:
+//       return state;
+//   }
+// };
 
-const reducer = (state: ToDos = [], action: Action) => {
-  switch (action.type) {
-    case addTodo.type:
-      console.log(action)
-      {
-        const newTodo: ToDo = { id: uuidv4(), todoTitle: action.payload };
-        return [newTodo, ...state];
-      }
-      break;
-    case deleteTodo.type:
-      return state.filter((todo) => todo.id !== action.payload);
-      break;
-    default:
-      return state;
-  }
-};
+// toolkit 이 reducer
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addTodo, (state, action) => {
+      state.push({ todoTitle: action.payload, id: uuidv4() });
+    })
+
+    .addCase(deleteTodo, (state, action) =>
+      state.filter((ToDo) => ToDo.id !== action.payload)
+    );
+});
 
 const todoStore = createStore(reducer);
 
